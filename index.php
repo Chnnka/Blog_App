eb327ee37831a8558514007257a83c495856bd8f
Channaka Gunawardhana <50627400+Chnnka@users.noreply.github.com>
2021-07-14 09:10:40 +0530

    I create my blog app

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="Plugin/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet">
+    
+    <script src="Plugin/jquery.min.js"></script>
+    <script src="Plugin/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Blog App</title>
+</head>
+<body>
+    <?php include_once('inc/navbar.php')?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron mt-4">
+                    <h4 id="jumbo-header">Welcome to the aesthetic world</h4>
+                    <h4 id="jumbo-emoji" style="text-align:center;">🎨</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
